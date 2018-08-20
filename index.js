7364119ee16c5beea4d338c02df7f3d0724b2a05
Steven Zapata <szapata427@gmail.com>
2018-08-20 12:22:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var cart = [];
 
-function getCart() {
+function getCart(name) {
  return cart;
 }
 
@@ -9,13 +9,39 @@ function setCart(c) {
   return cart;
 }
 
+// use that passed-in-string to create a new object representing the item
+// object should consister of two key value pairs
+// as more items are added to the cart it should look like 
+
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  var newobject = {itemName: item , itemPrice: price};
+  cart.push(newobject)
+  return (item + " has been added to your cart.");
 }
 
+// function does not accept any arguements
+// it should loop over every item in your cart
+// returning the constents as one long, coherent statement in this format:
+// "In your cart you have bananas at $17, pancake batter at $5, and eggs at $49."
+// if the car is empty, return, Your shopping car is empty
+// returned statement should be a single sentence that begins with "In your car, you have"
+  
 function viewCart() {
-  // write your code here
+  if (0 === cart.length) {
+    return ("Your shopping cart is empty.");
+  }
+  let statementArray = []
+  let i = 0, l = cart.length 
+  while(i < l) { 
+  var item = Object.values(cart[i])[0]
+  var price = Object.values(cart[i])[1]
+  statementArray.push(item +" at $" + price);
+  i++;
+  }
+  return(`In your cart, you have ${statementArray.join(', ')}.`)
 }
+
 
 function total() {
   // write your code here
